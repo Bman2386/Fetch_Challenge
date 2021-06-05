f317b676103c61d57e3aaecadfca22636629700c
Brendon <brendon.biagi@gmail.com>
2021-06-05 12:59:52 -0400

    setup controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do 
-    resources :users, only: [:index, :create, :show, :update, :destoy]
+    resources :users, only: [ :create, :show, :update, :destoy]
     resources :points, only: [:index, :new, :create, :show, :update, :destroy]
     resources :shops, only: [:index, :show, :update]
     resource :session, only: [:create, :destroy]
